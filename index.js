c764017f869cb4d96052757eeef79a4aed23d1eb
Samreensaba <Samren_saba@yahoo.com>
2022-06-09 16:01:44 +0100

    update

--- index.js
+++ index.js
@@ -21,10 +21,28 @@ window.onload=async()=>{
 
 
  
-const filterBooks = () =>{
+const filterBooks = async () =>{
     let text = document.getElementById("input").value
-    text = "hi sam"
-    console.log(text)
+    
+    const response = await fetch("https://striveschool-api.herokuapp.com/books")
+    const books = await response.json()
+    const filteredBooks = books.filter(book=>book.title === text)
+    console.log(filterBooks)
+        /*const bookCard = document.querySelector('.row')
+        const col = document.createElement('div')
+        col.className = "col-sm-6 col-md-4 col-lg-3 crd"
+        col.innerHTML = `<div class="card">
+                            <img src=${book.img} class="card-img-top" alt=${book.title}>
+                            <div class="card-body">
+                                <h5 class="card-title">${book.title}</h5>
+                                <p class="card-text">${book.category}</p>
+                                <button class="btn btn-primary crtbtn">Add to cart</button>
+                                <button class="btn btn-primary" onclick="skip(event)">Skip</button>
+                            </div>
+                            </div>
+        `
+        bookCard.appendChild(col)  */
+    
 }
 
 
