a5873b62f6f3cb643f5ff2b8f27a3371eada8a4f
Samreensaba <Samren_saba@yahoo.com>
2022-06-09 17:20:03 +0100

    update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 window.onload=async()=>{
+    try{
     const response = await fetch("https://striveschool-api.herokuapp.com/books")
     const books = await response.json()
+    allBooks = books
     books.forEach(book=>{
         const bookCard = document.querySelector('.row')
         const col = document.createElement('div')
@@ -10,7 +12,7 @@ window.onload=async()=>{
                             <div class="card-body">
                                 <h5 class="card-title">${book.title}</h5>
                                 <p class="card-text">${book.category}</p>
-                                <button class="btn btn-primary crtbtn">Add to cart</button>
+                                <button class="btn btn-primary crtbtn"; id="${book.asin}" onclick= "addToCart(event)">Add to cart</button>
                                 <button class="btn btn-primary" onclick="skip(event)">Skip</button>
                             </div>
                             </div>
@@ -18,32 +20,25 @@ window.onload=async()=>{
         bookCard.appendChild(col)  
     })
 }
-
-
- 
-const filterBooks = async () =>{
-    let text = document.getElementById("input").value
-    
-    const response = await fetch("https://striveschool-api.herokuapp.com/books")
-    const books = await response.json()
-    const filteredBooks = books.filter(book=>book.title === text)
-    console.log(filterBooks)
-        /*const bookCard = document.querySelector('.row')
-        const col = document.createElement('div')
-        col.className = "col-sm-6 col-md-4 col-lg-3 crd"
-        col.innerHTML = `<div class="card">
-                            <img src=${book.img} class="card-img-top" alt=${book.title}>
-                            <div class="card-body">
-                                <h5 class="card-title">${book.title}</h5>
-                                <p class="card-text">${book.category}</p>
-                                <button class="btn btn-primary crtbtn">Add to cart</button>
-                                <button class="btn btn-primary" onclick="skip(event)">Skip</button>
-                            </div>
-                            </div>
-        `
-        bookCard.appendChild(col)  */
+catch(err){
+    console.log(err)
+}
     
 }
+let allBooks= []
+
+const addToCart = (clickevent) =>{
+    let bookId = clickevent.target.id
+    //console.log(allBooks)
+    //console.log(clickedBtn)
+   let foundBook = allBooks.filter((book)=>{
+        return book.asin === bookId;
+    })[0]
+    //console.log(foundBook)
+    
+}
+ 
+
 
 
 
